3b7bae8c4961c86b34e9e5bed181d6f2df272f9a
Karsten Dambekalns <karsten@dambekalns.de>
2017-10-18 23:42:26 +0200

    TASK: Remove definition for Font VO from migration
    The Font VO is never stored in the persistence, thus the table is not needed.

--- Migrations/Postgresql/Version20171015140356.php
+++ Migrations/Postgresql/Version20171015140356.php
@@ -25,10 +25,6 @@ class Version20171015140356 extends AbstractMigration
     {
         $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'postgresql', 'Migration can only be executed safely on "postgresql".');
 
-        $this->addSql('CREATE TABLE cm_neos_thememodule_domain_model_font (family VARCHAR(255) NOT NULL, category VARCHAR(255) DEFAULT \'\', fontsource VARCHAR(255) NOT NULL, variants TEXT NOT NULL, subsets TEXT NOT NULL, files TEXT NOT NULL, PRIMARY KEY(family))');
-        $this->addSql('COMMENT ON COLUMN cm_neos_thememodule_domain_model_font.variants IS \'(DC2Type:array)\'');
-        $this->addSql('COMMENT ON COLUMN cm_neos_thememodule_domain_model_font.subsets IS \'(DC2Type:array)\'');
-        $this->addSql('COMMENT ON COLUMN cm_neos_thememodule_domain_model_font.files IS \'(DC2Type:array)\'');
         $this->addSql('CREATE TABLE cm_neos_thememodule_domain_model_settings (persistence_object_identifier VARCHAR(40) NOT NULL, customscss TEXT DEFAULT \'\', customcss TEXT DEFAULT \'\', customsettings TEXT DEFAULT \'\', PRIMARY KEY(persistence_object_identifier))');
     }
 
@@ -40,7 +36,6 @@ class Version20171015140356 extends AbstractMigration
     {
         $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'postgresql', 'Migration can only be executed safely on "postgresql".');
 
-        $this->addSql('DROP TABLE cm_neos_thememodule_domain_model_font');
         $this->addSql('DROP TABLE cm_neos_thememodule_domain_model_settings');
     }
 }
